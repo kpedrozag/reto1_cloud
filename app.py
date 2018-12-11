b184cb4eb815f2c5cb1da97467a2711d164af1be
jorgeality <jorgeality29@gmail.com>
2018-12-11 09:04:15 -0500

    add float values, 404 error handler

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify, redirect, url_for
 app = Flask(__name__) 
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "The URL does not exist."
+
 @app.route("/")
 def index():
 	return redirect(url_for("about"))
@@ -13,7 +17,7 @@ def index():
 def about():
 	return "Kevin Pedroza Goenaga"
 
-
+"""
 @app.route("/<string:op>/<int:a>/<int:b>")
 def ophtml(a,b,op):
 	if op == "sum":
@@ -38,6 +42,38 @@ def opjson(a,b,op):
 			return jsonify(message="Error: Division by zero not supported")
 	else:
 		return jsonify(message="Error in operation")
+# """
+
+
+@app.route("/<string:op>/<float:a>/<float:b>")
+@app.route("/<string:op>/<int:a>/<int:b>")
+def redirige(op,a,b):
+	return redirect(url_for('op_gen', op=op, a=a, b=b, format='html'))
+
+
+@app.route("/<string:op>/<float:a>/<float:b>/<string:format>")
+@app.route("/<string:op>/<int:a>/<int:b>/<string:format>")
+def op_gen(op,a,b,format='html'):
+	if op == "sum":
+		if format == 'json':
+			return jsonify(a=a, b=b, result=a+b)
+		elif format == 'html':
+			return "a = "+str(a)+", b = "+str(b)+", result = "+str(a+b)
+		else:
+			return "ERROR"
+	elif op == "divide":
+		if b != 0:
+			if format == 'json':
+				return jsonify(a=a, b=b, result=a/b)
+			elif format == 'html':
+				return "a = "+str(a)+", b = "+str(b)+", result = "+str(a/b)
+			else:
+				return "ERROR"
+		else:
+			return jsonify(message="Error: Division by zero not supported")
+	else:
+		return jsonify(message="Error in operation")
+
 
 
 
